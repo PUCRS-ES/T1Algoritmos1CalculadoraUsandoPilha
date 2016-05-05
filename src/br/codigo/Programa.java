f2e85b73f21f6ad17ae22f6a7850a6a9673e7db3
Vinicius Ferreira <vini.g.fer@gmail.com>
2016-05-05 15:47:50 -0300

    - Leitura do arquivo de expressoes

--- src/br/codigo/Programa.java
+++ src/br/codigo/Programa.java
@@ -1,9 +1,32 @@
 package br.codigo;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Programa {
 
 	public static void main(String[] args) {
-
+		Leitor leitor = null;
+		try {
+			String linhaAtual;
+			leitor = new Leitor("expressoes.txt");
+			do {
+				linhaAtual = leitor.leProximaLinha();
+				System.out.println(linhaAtual);
+			}
+			while(linhaAtual != null);
+		} catch (FileNotFoundException e) {
+			System.out.println("Arquivo não encontrado.");
+		} catch (IOException e) {
+			System.out.println("Erro na leitura do arquivo");
+		}
+		finally {
+			try {
+				leitor.fechaArquivo();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
